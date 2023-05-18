a4f92005629066c6295a80ada7e1b43d90e436a6
Gabriel Medeiros <gabriel.ma20@aluno.ifsc.edu.br>
2023-05-18 14:29:38 +0000

    atualização posição dos botões

--- cliente/js/cena-principal.js
+++ cliente/js/cena-principal.js
@@ -4,19 +4,124 @@ export default class principal extends Phaser.Scene {
     this.tabuleiro = [
       {
         numero: 0,
-        x: 10,
-        y: 300,
+        x: 15,
+        y: 260,
       },
       {
         numero: 1,
-        x: 60,
-        y: 300,
+        x: 15,
+        y: 465,
       },
       {
         numero: 2,
-        x: 110,
-        y: 300,
+        x: 15,
+        y: 667,
       },
+      {
+        numero: 3,
+        x: 220,
+        y: 260,
+      },
+      {
+        numero: 4,
+        x: 423,
+        y: 260,
+      },
+      {
+        numero: 5,
+        x: 423,
+        y: 465,
+      },
+      {
+        numero: 6,
+        x: 423,
+        y: 667,
+      },
+      {
+        numero: 7,
+        x: 220,
+        y: 667,
+      },
+      {
+        numero: 8,
+        x: 80,
+        y: 328
+      },
+      {
+        numero: 9,
+        x: 80,
+        y: 464,
+      },
+      {
+        numero: 10,
+        x: 80,
+        y: 607,
+      },
+      {
+        numero: 11,
+        x: 363,
+        y: 331
+      },
+      {
+        numero: 12,
+        x: 363,
+        y: 472
+      },
+      {
+        numero: 13,
+        x: 363,
+        y: 611,
+      },
+      {
+        numero: 14,
+        x: 224,
+        y: 331,
+      },
+      {
+        numero: 15,
+        x: 221,
+        y: 607,
+      },
+      {
+        numero: 16,
+        x: 145,
+        y: 391,
+      },
+      {
+        numero: 17,
+        x: 145,
+        y: 472,
+      },
+      {
+        numero: 18,
+        x: 145,
+        y: 550,
+      },
+      {
+        numero: 19,
+        x: 302,
+        y: 391,
+      },
+      {
+        numero: 20,
+        x: 302,
+        y: 472,
+      },
+      {
+        numero: 21,
+        x: 302,
+        y: 550,
+      },
+      {
+        numero: 22,
+        x: 224,
+        y: 391,
+      },
+      {
+        numero: 23,
+        x: 224,
+        y: 550,
+      }
     ];
   }
 
